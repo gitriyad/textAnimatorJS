a6eaf2a355dd0360d4c4439cd43a8a28fec68dd2
gitriyad <riyad.github@gmail.com>
2020-04-04 12:45:11 +0600

    css modified

--- js/textAnimator.js
+++ js/textAnimator.js
@@ -14,4 +14,8 @@ function textAnim(text, elem) {
         span.style.animation = "clo " + 0.2 * strlenght + "s linear " + delay + "s infinite";
         delay = delay + 0.2;
     }
+    var head = document.getElementsByTagName("head")[0];
+    var style = document.createElement("style");
+    style.innerHTML = "@-webkit-keyframes clo {0%{color: gray;}50%{color: white;}100%{color: gray;}}@keyframes clo {0%{color: gray;}50%{color: white;}100%{color: gray;}}#a{height:250px;width:500px;background:-webkit-gradient(linear,left top,right top,from(black),to(seagreen));background:linear-gradient(to right,#000,#2e8b57);margin:0 auto;text-align:center;text-transform:uppercase;line-height:250px}";
+    head.appendChild(style);
 }
